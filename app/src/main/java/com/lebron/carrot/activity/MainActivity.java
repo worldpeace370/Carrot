4f9d18a32146239d6640c01b9ea886128126f935
worldpeace370 <337360287@qq.com>
2016-03-31 20:46:38 +0800

    更新依赖

--- app/src/main/java/com/lebron/carrot/activity/MainActivity.java
+++ app/src/main/java/com/lebron/carrot/activity/MainActivity.java
@@ -13,7 +13,6 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.graphics.Palette;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
